27bd36b26dfec0ef290d79c2d345e2329e3d24aa
dachen <dachen@tencent.com>
2024-08-27 15:10:38 +0800

    using command for encap specific ansi convert string

---
+++ src/command.rs
@@ -0,0 +1,22 @@
+use crossterm::Command;
+
+pub enum OpsCommandType {
+    ClearToEnd,
+    UpOneLine,
+}
+
+pub struct OpsCommand(pub OpsCommandType);
+
+impl Command for OpsCommand {
+    fn write_ansi(&self, f: &mut impl std::fmt::Write) -> std::fmt::Result {
+        f.write_str(match self.0 {
+            OpsCommandType::ClearToEnd => "\x1b[K",
+            OpsCommandType::UpOneLine => "\x1b[F",
+        })
+    }
+
+    #[cfg(windows)]
+    fn execute_winapi(&self) -> io::Result<()> {
+        sys::clear(self.0)
+    }
+}
